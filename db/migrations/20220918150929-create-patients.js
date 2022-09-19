7d3caee69a572af0bf741abb1a74de1a60c4ec1b
siti haryati <sitiharyati614@gmail.com>
2022-09-19 16:15:09 +0700

    pull main

---
+++ db/migrations/20220918150929-create-patients.js
@@ -0,0 +1,60 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('patients', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      dateOfBirth: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      genderId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "genders",
+          },
+          key: "id",
+        },
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      NIK: {
+        type: Sequelize.INTEGER
+      },
+      BPJS: {
+        type: Sequelize.INTEGER
+      },
+      phoneNumber: {
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('patients');
+  }
+};
