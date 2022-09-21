3576e984ed8d7db93ef85ea41b7381ff7bd5d7a9
Safira Tyas Wandita <safeeratyas@gmail.com>
2022-09-21 11:58:51 +0700

    add: seeders for examination and gender

---
+++ db/seeders/20220921045542-insert-gender.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const genders = [
+  "Wanita",
+  "Pria"
+];
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const gender = genders.map((gen) => ({
+      name: gen,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }));
+
+    await queryInterface.bulkInsert('Genders', gender, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Genders', null, {});
+  }
+};
