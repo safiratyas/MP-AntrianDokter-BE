f7a081d4ecaa642ff605cb580e8194ed2926f578
Safira Tyas Wandita <safeeratyas@gmail.com>
2022-10-06 13:58:52 +0700

    add: examination by id

--- app/controllers/api/examinations.js
+++ app/controllers/api/examinations.js
@@ -13,4 +13,20 @@ module.exports = {
       data: getAll
     });
   },
+  
+  async getExaminations(req, res) {
+    try {
+      const examination = examinationsServices.get({
+        where: {
+          id
+        }
+      });
+      res.status(200).json(examination);
+    } catch (err) {
+      res.status(404).json({
+        status: 'Failed',
+        message: err.message
+      })
+    }
+  }
 };
