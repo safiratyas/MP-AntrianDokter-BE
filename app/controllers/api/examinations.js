4f0a36a6fd31b5a6346de4447580f4a1ff4a54d9
siti haryati <sitiharyati614@gmail.com>
2022-10-06 14:23:03 +0700

    update Readme and update examinations

--- app/controllers/api/examinations.js
+++ app/controllers/api/examinations.js
@@ -13,4 +13,25 @@ module.exports = {
       data: getAll
     });
   },
+  
+  async getExaminations(req, res) {
+    try {
+      const examinations = await examinationsServices.getOne({
+        where: {
+          id: req.params.id
+        },
+      });
+
+      if (!examinations) {
+        throw new Error(`Jenis pengecekan dengan ID ${req.params.id} tidak ditemukan!`);
+      }
+
+      res.status(200).json(examinations);
+    } catch (err) {
+      res.status(404).json({
+        status: 'Failed',
+        message: err.message,
+      });
+    }
+  },
 };
